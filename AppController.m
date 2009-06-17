c19cf8d3695bc99b154c7755e9c45f73d322f152
Wesley Moore <wez@wezm.net>
2009-06-17 13:05:07 +1000

    Copy generated password to the clipboard

--- AppController.m
+++ AppController.m
@@ -5,8 +5,14 @@
 
 - (IBAction)generatePassword:(id)sender {
     WMPasswordComposer *composer = [[WMPasswordComposer alloc] init];
-	
-	NSLog(@"Password %@", [composer generatePasswordForDomain:[domain stringValue] withMasterPassword:[master_password stringValue]]);
+	NSString *pw = [composer generatePasswordForDomain:[domain stringValue] withMasterPassword:[master_password stringValue]];
+	NSPasteboard *pasteboard = [NSPasteboard pasteboardWithName:NSGeneralPboard];
+
+	[pasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:self];
+	[pasteboard setString:pw forType:NSStringPboardType];
+
+	[message_label setStringValue:@"Copied"];
+	[message_label setHidden:NO];
 	
 	[composer release];
 }
